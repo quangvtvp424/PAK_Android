8761f7ee8fc004cdb900debd56244eb43060a0be
quanglm <quanglm@quanglms-MacBook-Pro.local>
2017-10-04 22:55:29 +0700

    add practice project

---
+++ PAK_Android_HN/Practice/PAKAndroidPractice/app/src/main/java/com/ptit/pakandroidpractice/lesson03/Lesson03Activity.java
@@ -0,0 +1,15 @@
+package com.ptit.pakandroidpractice.lesson03;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ptit.pakandroidpractice.R;
+
+public class Lesson03Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
