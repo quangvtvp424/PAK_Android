18d17da37613f75c122f6d0cf6327836b43e7635
quanglm <quanglm@quanglms-MacBook-Pro.local>
2017-10-04 22:21:51 +0700

    commit source code

---
+++ PAK_Android_HN/Training/Sourcecode/app/src/main/java/com/ptit/pak_android_course/lesson03/MainActivity.java
@@ -0,0 +1,71 @@
+package com.ptit.pak_android_course.lesson03;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.ptit.pak_android_course.R;
+
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void intentAction(View view) {
+//        intentDialNumber();
+        showActionSend();
+    }
+
+    private void intentDialNumber() {
+        Uri number = Uri.parse("tel:0914471189");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+//        testQueryIntent(callIntent);
+        startActivity(callIntent);
+
+
+    }
+
+    private void showActionSend() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, "my app"));
+    }
+
+    private void testQueryIntent(Intent intent) {
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+
+        Log.d(TAG, "testQueryIntent: isIntentSafe = " + isIntentSafe);
+        if (isIntentSafe) {
+            for (ResolveInfo info : activities)
+            Log.d(TAG, "testQueryIntent: activities matched: " + info.activityInfo.name);
+        }
+
+    }
+
+    public void openWelcomeScreen(View view) {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        intent.putExtra("username", "Admin");
+        startActivity(intent);
+    }
+
+
+
+}
---
+++ PAK_Android_HN/Training/Sourcecode/app/src/main/java/com/ptit/pak_android_course/lesson03/WelcomeActivity.java
@@ -0,0 +1,50 @@
+package com.ptit.pak_android_course.lesson03;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.ptit.pak_android_course.R;
+
+/**
+ * Created by quanglm on 9/30/17.
+ */
+
+public class WelcomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Intent i = getIntent();
+        if (i != null) {
+            String userName = i.getExtras().getString("username");
+            ((TextView) findViewById(R.id.textView)).setText(userName);
+            ((EditText) findViewById(R.id.editText)).setText(userName);
+            String textExtra = i.getExtras().getString(android.content.Intent.EXTRA_TEXT);
+            if (!TextUtils.isEmpty(textExtra)) {
+                ((TextView) findViewById(R.id.textView)).setText(textExtra);
+            }
+        } else {
+            ((TextView)findViewById(R.id.textView)).setText("Have Nothing!");
+        }
+    }
+
+    public void showDialog(View view) {
+
+//        new AlertDialog.Builder(this)
+//                .setMessage("Testing activity lifecycle!")
+//                .setPositiveButton("OK", null)
+//
+//                .show();
+
+        Intent i = new Intent(this, DialogActivity.class);
+        startActivity(i);
+    }
+}
