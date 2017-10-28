757b58510ddcb26452ff1ab2b02c4a90cd953752
Le Thanh Binh <smigence@gmail.com>
2017-10-28 23:32:31 +0700

    lesson4_v1

---
+++ PAK_Android_HN/Practice/PAKAndroidPractice/app/src/main/java/com/ptit/pakandroidpractice/lesson4/Rectangle.java
@@ -0,0 +1,8 @@
+package com.ptit.pakandroidpractice.lesson4;
+
+/**
+ * Created by AM on 10/28/2017.
+ */
+
+public class Rectangle extends Shape {
+}
---
+++ PAK_Android_HN/Practice/PAKAndroidPractice/app/src/main/java/com/ptit/pakandroidpractice/lesson4/Shape.java
@@ -0,0 +1,8 @@
+package com.ptit.pakandroidpractice.lesson4;
+
+/**
+ * Created by AM on 10/28/2017.
+ */
+
+public abstract class Shape {
+}
