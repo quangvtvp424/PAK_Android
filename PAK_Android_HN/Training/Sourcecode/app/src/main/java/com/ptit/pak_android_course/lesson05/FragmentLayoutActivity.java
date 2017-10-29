02dbd4f3f620338473206b81d291512c64ba7895
Nguyen duc thuan <nguyenducthuan24@gmail.com>
2017-10-29 08:11:50 +0700

    demo for fragment

---
+++ PAK_Android_HN/Training/Sourcecode/app/src/main/java/com/ptit/pak_android_course/lesson05/FragmentLayoutActivity.java
@@ -0,0 +1,22 @@
+package com.ptit.pak_android_course.lesson05;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import com.ptit.pak_android_course.R;
+
+/**
+ * Created by Thuannd on 10/29/2017.
+ */
+
+public class FragmentLayoutActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+    }
+
+
+}
