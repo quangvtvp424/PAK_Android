ef6e270a935beb6f04280c6cac87eee68f44f055
quanglm <quanglm@quanglms-MacBook-Pro.local>
2017-12-16 19:52:00 +0700

    update google map key

--- PAK_Android_HN/Training/Sourcecode/app/src/main/java/com/ptit/pak_android_course/lesson08/MapsActivityLess08.java
+++ PAK_Android_HN/Training/Sourcecode/app/src/main/java/com/ptit/pak_android_course/lesson08/MapsActivityLess08.java
@@ -3,9 +3,15 @@ package com.ptit.pak_android_course.lesson08;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ptit.pak_android_course.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivityLess08 extends FragmentActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
